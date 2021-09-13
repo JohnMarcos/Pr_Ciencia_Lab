01e7968273104e828573baeac380f4df105ca727
john videla <john.marcos.videla.r@gmail.com>
2021-09-13 17:07:51 -0300

    Se agrega modelo Comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_200505) do
+ActiveRecord::Schema.define(version: 2021_09_13_200717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
