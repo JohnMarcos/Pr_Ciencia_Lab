2412d004665b2fec75565cc2a48b7d31f1db49bd
john videla <john.marcos.videla.r@gmail.com>
2021-09-14 14:08:53 -0300

    Se agregan routes

--- db/migrate/20210914123739_add_user_to_comments.rb
+++
@@ -1,4 +0,0 @@
-class AddUserToComments < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
--- db/migrate/20210914131326_add_last_name_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLastNameToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :users, :last_name, null: false, foreign_key: true
-  end
-end
